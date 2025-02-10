b78ddfc9eff321b1a452d05587541d742f8fc61b
Van5sh <vansh05dhir@gmail.com>
2025-02-10 14:40:46 +0530

    svg's added

---
+++ src/app/components/firstpage.tsx
@@ -0,0 +1,11 @@
+import icon1 from "@/app/assets/icon1.svg"
+import icon2 from "@/app/assets/icon2.svg"
+import Image from "next/image";
+
+export default function Firstpage(){
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -3,12 +3,14 @@ import "../globals.css";
 import Footer from "@/app/footer";
 import FourthPage from "@/app/components/fourthpage";
 import ThirdPage from "@/app/components/thirdpage";
+import FirstPage from "@/app/components/firstpage";
 import SecondPage from "@/app/components/secondpage";
 
 export default function Landing() {
     return (
         <div className="bg-white min-h-screen">
             <Navbar />
+            <FirstPage/>
             <SecondPage/>
             <ThirdPage/>
             <FourthPage/>
